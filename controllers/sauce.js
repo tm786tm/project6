903a26047c2829430b5fa113d8c3a5f6ec0b13e9
tm786tm <tm786tm@gmail.com>
2020-07-09 22:00:51 +0100

    Got like and dislike working

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -49,6 +49,99 @@ exports.getOneSauce = (req, res, next) => {
     );
 };
 
+
+exports.likeSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then(
+        (sauce) => {
+            if (req.body.like === 1) {
+                if (sauce.usersLiked.length === 0) {
+                    sauce.usersLiked.push(req.body.userId);
+                    sauce.likes += 1;
+                } else {
+                    let alreadyLiked = false;
+                    for (let i = 0; i < sauce.usersLiked.length; i++) {
+                        if (sauce.usersLiked[i] === req.body.userId) {
+                            alreadyLiked = true;
+                        }
+                    }
+                    if (!alreadyLiked) {
+                        sauce.likes += 1;
+                        console.log('New Like');
+                        sauce.usersLiked.push(req.body.userId);
+                    }
+                }
+            }
+
+            if (req.body.like === -1) {
+                if (sauce.usersDisliked.length === 0) {
+                    sauce.usersDisliked.push(req.body.userId);
+                    sauce.dislikes += 1;
+                } else {
+                    let alreadyDisliked = false;
+                    for (let i = 0; i < sauce.usersDisliked.length; i++) {
+                        if (sauce.usersDisliked[i] === req.body.userId) {
+                            alreadyDisliked = true;
+                        }
+                    }
+                    if (!alreadyDisliked) {
+                        sauce.dislikes += 1;
+                        console.log('New dislike');
+                        sauce.usersDisliked.push(req.body.userId);
+                    }
+                }
+            }
+
+            if (req.body.like === 0) {
+                if (sauce.usersLiked.length > 0) {
+                    for (let i = 0; i < sauce.usersLiked.length; i++) {
+                        if (sauce.usersLiked[i] === req.body.userId) {
+                            sauce.likes -= 1;
+                            sauce.usersLiked.splice(i, 1);
+                            console.log('Removed like');
+                        }
+                    }
+                }
+                if (sauce.usersDisliked.length > 0) {
+                    for (let i = 0; i < sauce.usersDisliked.length; i++) {
+                        if (sauce.usersDisliked[i] === req.body.userId) {
+                            sauce.dislikes -= 1;
+                            sauce.usersDisliked.splice(i, 1);
+                            console.log('Removed dislike');
+                        }
+                    }
+                }
+            }
+
+            console.log(sauce.likes + ' like ' + 'and ' + sauce.dislikes + ' dislikes.');
+            console.log('Liked by ' + sauce.usersLiked);
+            console.log('Disliked by ' + sauce.usersDisliked);
+            console.log('-----------------------------------');
+            Sauce.updateOne({ _id: req.params.id }, sauce).then(
+                () => {
+                    res.status(201).json({
+                        message: 'Thing updated successfully!'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(400).json({
+                        error: error
+                    });
+                }
+            );
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+
+};
+
 exports.modifySauce = (req, res, next) => {
     let sauce = new Sauce({ _id: req.params._id });
     if (req.file) {
--- routes/sauce.js
+++ routes/sauce.js
@@ -9,6 +9,7 @@ const sauceCtrl = require('../controllers/sauce');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
